6224b3307391b214d59f8df3fc20d328c6fd3c89
ReahKy23 <93449477+ReahKy23@users.noreply.github.com>
2022-05-10 16:54:50 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//kylah kerr 
+//kylah.kerr34@myhunter.cuny.edu
+//10 May 2022
+//Converts Miles to Kilometers
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{ 
+    float km, Miles;
+    cout << "Enter km: ";
+    cin >> km;
+    Miles = km * 0.621371;
+    cout << endl << "Miles:" << Miles << "\n\n";
+    return 0;
+}
